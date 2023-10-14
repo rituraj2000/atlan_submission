674a076e9ab7da5ad9e72107f290a93a50076552
Rituraj Tripsthy <rituraj.tripathy@gmail.com>
2023-10-15 04:24:23 +0530

    history implemented

--- src/Pages/Home/components/SQLbar.js
+++ src/Pages/Home/components/SQLbar.js
@@ -1,16 +1,44 @@
+import React, { useState, useRef, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHistory } from "@fortawesome/free-solid-svg-icons";
 
-// SearchBar.js
 export const SQLBar = ({
   searchTerm,
   setSearchTerm,
-  setTable,
+  setSQLQuery,
   handleQuery,
 }) => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleRunQuery = () => {
+    if (!history.includes(searchTerm)) {
+      setHistory([...history, searchTerm]);
+    }
+    handleQuery();
+  };
+
+  const handleHistoryClick = (query) => {
+    setSearchTerm(query);
+    setDropdownOpen(false);
+  };
+
   return (
-    <div className="mb-1 ml-4 w-full flex items-center">
-      {/* Input field */}
+    <div className="mb-1 ml-4 w-full flex items-center relative">
       <div className="py-2 px-8 w-full border rounded-xl italic text-slate-400 flex items-center">
         <input
           type="text"
@@ -20,20 +48,39 @@ export const SQLBar = ({
           onChange={(e) => setSearchTerm(e.target.value)}
         />
         <button
-          className=" h-8 w-8 ml-2 bg-white rounded-full hover:bg-gray-100 text-sm"
-          title="View History" // This will show a tooltip on hover
+          onClick={() => setDropdownOpen(!isDropdownOpen)}
+          className="h-8 w-8 ml-2 bg-white rounded-full hover:bg-gray-100 text-sm"
+          title="View History"
         >
           <FontAwesomeIcon icon={faHistory} />
         </button>
       </div>
+      
+      <div ref={dropdownRef}>
+        {isDropdownOpen && (
+          <div
+            className="absolute bg-white mt-2 w-64 rounded-md shadow-lg border"
+            style={{ right: "10rem" }}
+          >
+            <ul>
+              {history.map((query, index) => (
+                <li
+                  key={index}
+                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handleHistoryClick(query)}
+                >
+                  {query}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
 
-      {/* Button */}
       <button
-        onClick={() => {
-          handleQuery();
-        }}
+        onClick={handleRunQuery}
         className="bg-[#468AF9] text-white font-extralight mr-7 rounded-md py-2 px-4 m-2 inline-block whitespace-nowrap hover:shadow-lg hover:bg-gradient-to-r hover:from-[#468AF9] hover:to-[#357abD]"
-        style={{ height: "100%" }} // Set height to match input field
+        style={{ height: "100%" }}
       >
         Run Query
       </button>
